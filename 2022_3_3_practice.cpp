95c7a29cf18e64a174580890f4acad19ff4d142d
CCLCK <84182037+CCLCK@users.noreply.github.com>
2022-03-03 23:59:24 +0800

    找最近的公共祖先+二进制连续1的最大个数

--- 2022_3_3_practice.cpp
+++ 2022_3_3_practice.cpp
@@ -139,52 +139,7 @@ using namespace std;
 
 
 
-//class ClassA
-//{
-//public:
-//	ClassA()
-//	{
-//		cout << "ClassA()" << endl;
-//	}
-//	~ClassA()
-//	{
-//		cout << "~ClassA()" << endl;
-//	}
-//};
-//int main()
-//{
-//	ClassA* pclassA = new ClassA[5];
-//	cout << sizeof(ClassA) << endl;
-//	delete pclassA;
-//	/*delete[] pclassA;*/
-//	return 0;
-//}
 
-
-#include<iostream>
-using namespace std;
-class A
-{
-public:
-	A()
-	{
-		cout << "A()" << endl;
-	}
-	~A()
-	{
-		cout << "~A()" << endl;
-	}
-private:
-	int b;
-	int& c= b;
-	const int d=1;
-};
-int main()
-{
-	A* a = new A;
-	//A aa;
-	return 0;
-}
 
 
 
