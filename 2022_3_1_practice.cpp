1bb79267a97ae8d60845a2070a04757f52c1453a
CCLCK <84182037+CCLCK@users.noreply.github.com>
2022-04-01 10:53:50 +0800

    养兔子（简单dp）+水题

--- 2022_3_1_practice.cpp
+++ 2022_3_1_practice.cpp
@@ -2,70 +2,43 @@
 
 
 
-
-
-
-
-
-//#include<iostream>
+//#include <iostream>
+//#include <string>
 //using namespace std;
-//int n, m;
-//int a[10][10];
-//int cnt = 0;
-//void dfs(int x,int y)
-//{
-//	if (x == n&&y == m)
-//	{
-//		cnt++;
-//		return;
-//	}
-//	if (a[x][y] == 0 && x >= 0 && x <= n&&y >= 0 && y <= m)
-//	{
-//		a[x][y] = 1;
-//		dfs(x + 1, y);
-//		dfs(x, y + 1);
-//		a[x][y] = 0;
-//	}
-//	else
-//	{
-//		return;
-//	}
-//	
-//}
 //
 //int main()
 //{
-//	while (cin >> n >> m)
+//	int n;
+//	while (cin >> n)
 //	{
-//		memset(a, 0, sizeof(a));
-//		cnt = 0;
-//		dfs(0, 0);
-//		cout << cnt << endl;
+//		getchar();
+//		string s;
+//		for (int i = 0; i < n-1; i++)
+//		{		
+//			getline(cin, s);
+//			if (s.find(" ") != string::npos || s.find(",") != string::npos)
+//			{
+//				cout << '"' << s << '"' << ", ";
+//			}
+//			else
+//			{
+//				cout << s << ", ";
+//			}
+//		}
+//		getline(cin, s);
+//		if (s.find(" ") != string::npos || s.find(",") != string::npos)
+//		{
+//			cout << '"' << s << '"' << endl;
+//		}
+//		else
+//		{
+//			cout << s << endl;
+//		}
 //	}
 //	return 0;
 //}
 
 
 
-//class Solution {
-//public:
-//	int add(int a, int b) {
-//		while (b)
-//		{
-//不用加号实现加法
-//			int carry = (a&b) << 1;
-//			a = a^b;
-//			b = carry;
-//		}
-//		return a;
-//	}
-//};
-//#include<iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int a = -1;
-//	cout << (a << 1) << endl;
-//	cout << Solution().add(-1, 2) << endl;
-//}
+
+
